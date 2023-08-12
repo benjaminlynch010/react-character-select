98c6b34831eaf2af548b6b7d5abc5eccef5b7dc8
Ben Lynch <bdlynch7@gmail.com>
2023-08-12 18:35:09 -0500

    fixed img path

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -6,7 +6,7 @@ const galleryItems = [
   { id: 5, path: 'images/Bridget.png', description: 'Bridget', likes: 0 },
   { id: 6, path: 'images/Millia.png', description: 'Millia Rage', likes: 0 },
   { id: 7, path: 'images/Venom.png', description: 'Venom', likes: 0 },
-  { id: 8, path: 'images/Zato', description: 'Zato-One / Eddie', likes: 0 }
+  { id: 8, path: 'images/Zato.png', description: 'Zato-One / Eddie', likes: 0 }
 ];
 
 module.exports = galleryItems;
