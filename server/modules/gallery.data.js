744d63d5fe63031549ec963b1e31e7993b52ee38
Ben Lynch <bdlynch7@gmail.com>
2023-08-15 09:42:02 -0500

    gif test

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -6,7 +6,8 @@ const galleryItems = [
   { id: 5, path: 'images/Bridget.png', description: 'Bridget', likes: 0 },
   { id: 6, path: 'images/Millia.png', description: 'Millia Rage', likes: 0 },
   { id: 7, path: 'images/Venom.png', description: 'Venom', likes: 0 },
-  { id: 8, path: 'images/Zato.png', description: 'Zato-One / Eddie', likes: 0 }
+  { id: 8, path: 'images/Zato.png', description: 'Zato-One / Eddie', likes: 0 },
+  { id: 9, path: 'images/sol-explode.gif', description: 'Tyrant Rave', likes: 0 }
 ];
 
 module.exports = galleryItems;
